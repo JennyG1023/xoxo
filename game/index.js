d13dcb3404d32be4fcedff2c4e134502742a0bc5
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2018-05-16 14:16:13 -0400

    first part done

--- game/index.js
+++ game/index.js
@@ -1,6 +1,29 @@
-import {Map} from 'immutable'
+import { Map } from "immutable";
+import { createStore } from "redux";
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map();
+
+//Type
+const MOVE = "MOVE";
+
+//Action Creator
+const move = (turn, coordinates) => {
+  board = board.setIn(coordinates, turn);
+  return {
+    type: MOVE,
+    player: turn,
+    board: board
+  };
+};
+
+export default function gameReducer(state = { board, turn: "_" }, action) {
+  switch (action.type) {
+    case MOVE:
+      return { turn: action.player, board: action.board };
+    default:
+      return state;
+  }
 }
+
+//Creating the store, which is the state
+// const game = createStore(reducer);
